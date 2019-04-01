ea2a4dcb80d1934e51ad13c42251c0c5750e50c6
luizpaulosilva <luiz.paulo.09@hotmail.com>
2019-03-31 22:56:44 -0300

    Displaying a List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+// ANGULAR
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { BrowserModule } from '@angular/platform-browser';
 
+// NOSSO
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeroesComponent } from './heroes/heroes.component';
