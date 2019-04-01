9c47b5dfa4f680d4b7fa5286e709d4e10be5d3ed
luizpaulosilva <luiz.paulo.09@hotmail.com>
2019-03-31 22:02:59 -0300

    Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Heroe } from '../model/hero';
+import { Hero } from '../model/hero';
+import { HEROES } from './../shared/mock/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +10,31 @@ import { Heroe } from '../model/hero';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Heroe[];
-  hero: Heroe;
+  hero: Hero;
+  heroes: Hero[];
+  selectedHero: Hero;
+  heroesMock: Hero[] = HEROES;
 
   constructor() { }
 
   ngOnInit() {
     this.heroes = [];
-    this.heroes.push(this.hero = new Heroe(1, 'Capitão América', 'Força Sobrenatural', new Date()));
+    this.heroes.push(this.hero = new Hero(1, 'Capitão América', 'Força Sobrenatural', new Date()));
     this.heroes.push({ id: 2, name: 'Batman', power: 'Homem Morcego', dateRegister: new Date() });
+  }
+
+  onSelectHero(hero: Hero): void {
+    if (hero !== undefined && hero.id !== 0) {
+      this.selectedHero = hero;
+      console.log('O Herói informado é: ' + hero.id + ' - ' + hero.name);
+    }
+  }
+
+  selectedHeroIsNotNull(): boolean {
+    if (this.selectedHero !== null && this.selectedHero !== undefined) {
+      return true;
+    }
+    return false;
   }
 
 }
---
+++ src/app/shared/mock/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './../../model/hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice', power: 'Force', dateRegister: new Date() },
+  { id: 12, name: 'Narco', power: 'Force', dateRegister: new Date() },
+  { id: 13, name: 'Bombasto', power: 'Force', dateRegister: new Date() },
+  { id: 14, name: 'Celeritas', power: 'Force', dateRegister: new Date() },
+  { id: 15, name: 'Magneta', power: 'Force', dateRegister: new Date() },
+  { id: 16, name: 'RubberMan', power: 'Force', dateRegister: new Date() },
+  { id: 17, name: 'Dynama', power: 'Force', dateRegister: new Date() },
+  { id: 18, name: 'Dr IQ', power: 'Force', dateRegister: new Date() },
+  { id: 19, name: 'Magma', power: 'Force', dateRegister: new Date() },
+  { id: 20, name: 'Tornado', power: 'Force', dateRegister: new Date() }
+];
